09fba73f7e6e369e7ccf0f389869332786cd5eb5
Rob Egan <rsegan@lbl.gov>
2017-04-11 23:31:41 -0700

    fixed indent

--- poretools/statistics.py
+++ poretools/statistics.py
@@ -27,25 +27,25 @@ def median(l):
 		return None
 
 def NX(l, x=[25,50,75]):
-        """
-        Returns NX for all x for a list of numbers l.
-        Default: N25, N50, N75
-        Assumes all values in list x are between 0 and 100.
-        Interpretation: When NX = NX_value, X% of data (in bp) is contained in reads at least NX_value bp long.
-        """
+	"""
+	Returns NX for all x for a list of numbers l.
+	Default: N25, N50, N75
+	Assumes all values in list x are between 0 and 100.
+	Interpretation: When NX = NX_value, X% of data (in bp) is contained in reads at least NX_value bp long.
+	"""
 	if isinstance(l, list) and isinstance(x, list):
 		l = sorted(l)
 		x = sorted(x)
 		total = sum(l)
-                nxsum = 0
-                nxvalues = {e:0 for e in x}
+		nxsum = 0
+		nxvalues = {e:0 for e in x}
 		for e in x:
-                        xpct = total*e/100.0
-                        while nxsum < xpct and l:
-                                nxsum += l[-1]
-                                lastsize = l.pop()
-                        nxvalues[e] = lastsize
-                return nxvalues
+			xpct = total*e/100.0
+			while nxsum < xpct and l:
+				nxsum += l[-1]
+				lastsize = l.pop()
+			nxvalues[e] = lastsize
+		return nxvalues
 
 	else:
 		return None
